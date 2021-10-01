52b75d3ec3f9841266aabdec69d17c179a3a9c64
Aayush <falcon.bridge2000@gmail.com>
2021-10-01 10:38:36 +0530

    added new expense form component and delivered its data up to the tracker app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
 const App = () => {
   const expenses = [
     {
@@ -28,36 +30,18 @@ const App = () => {
   //   React.createElement("h2", {}, "Let's get started!"),
   //   React.createElement(Expenses, { expenses: expenses })
   // );
+
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses expenses={expenses} />
     </div>
   );
 };
 
 export default App;
-
-// <ExpenseItem /> = <ExpenseItem></ExpenseItem> if there is nothing b/w opening and closing tags
-{
-  /* <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      /> */
-}
