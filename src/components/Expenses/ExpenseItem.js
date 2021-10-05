557e0e9e1791abaf96ff3eacf4b8d9ccbce38630
Aayush <falcon.bridge2000@gmail.com>
2021-10-05 15:18:17 +0530

    added expense year dropdown menu and removed the dummy change title button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,23 @@
-import React,{useState} from "react";
+import React from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  const [title,setTitle]=useState(props.title);
-  // const title = props.title;
+  // const [title,setTitle]=useState(props.title);
+  const title = props.title;
   const expenseAmount = props.amount;
-  const clickHandler=()=>{
-    setTitle("Updated!!!");
-    console.log(title);
-  }
+  // const clickHandler=()=>{
+  //   setTitle("Updated!!!");
+  //   console.log(title);
+  // }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
-        <button onClick={clickHandler}>Change Title</button>
+        {/* <button onClick={clickHandler}>Change Title</button> */}
       </div>
     </Card>
   );
