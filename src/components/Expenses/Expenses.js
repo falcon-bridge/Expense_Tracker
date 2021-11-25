e1e47958c9be7d929710647c72445139315deaba
Aayush <falcon.bridge2000@gmail.com>
2021-11-25 19:36:48 +0530

    added the chart to have the visual representation of expemses in the chosen year and fixed a small bug (entered amount from the form was previously submitted by the form as strings which should have been as a number)

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,6 +5,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const expenses = props.expenses;
@@ -26,6 +27,9 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+
+        <ExpensesChart expenses={filteredExpenses} />
+
         {/* <ExpenseItem
           title={expenses[0].title}
           amount={expenses[0].amount}
